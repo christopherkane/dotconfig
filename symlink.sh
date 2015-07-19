622b81f7fc0bef3486365ccc4fd81c66f0289914
Christopher Kane <ckane@debian>
2015-07-19 17:08:18 +0100

    Added configuration script

---
+++ symlink.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Get absolute path of directory
+DIR=$( cd "$( dirname "$0" )" && pwd )
+
+# Add symbolic links
+ln -sf ${DIR}/dotbashrc ~/.bashrc
+ln -sf ${DIR}/dotvimrc ~/.vimrc
+ln -sf ${DIR}/dotvim ~/.vim
+ln -sf ${DIR}/dotfonts ~/.fonts
