899ea275afca4d066be39f0e6bb195bee1d20dc3
Christopher Kane <ckane386@gmail.com>
2018-02-06 16:30:40 +0000

    Added global gitignore, with a focus on Java, C, and Pythonm

--- symlink.sh
+++ symlink.sh
@@ -8,3 +8,7 @@ ln -sf ${DIR}/dotbashrc ~/.bashrc
 ln -sf ${DIR}/dotvimrc ~/.vimrc
 ln -sf ${DIR}/dotvim ~/.vim
 ln -sf ${DIR}/dotfonts ~/.fonts
+ln -sf ${DIR}/dotgitignore_global ~/.gitignore_global
+
+# Install global gitignore
+git config --global core.excludesfile ~/.gitignore_global
